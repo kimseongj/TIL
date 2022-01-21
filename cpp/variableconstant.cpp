d27379c92a54fb09071e42f27c7c44e1f7344b62
name <ksj8277@naver.com>
2022-01-21 15:53:05 +0900

    커밋 테스트 및 C++ 공부
    cin / 추출 연산자 / 삽입 연산자
    타입 (변수 / 상수)

---
+++ cpp/variableconstant.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+
+int main() 
+{
+    int num;  // 변수 선언
+    num = 20;
+
+    int num1, num2;
+    double num3 = 1.23, num4 = 4.56;
+
+    int a = 10;
+
+    cout << "숫자 10을 10진수로 표현하면" << a << "이며," <<endl;
+    cout << oct;
+    cout << "숫자 10을 8진수로 표현하면" << a << "이며," << endl;
+    cout << hex;
+    cout << "숫자 10을 16진수로 표현하면" << a << "입니다.";
+    
+    const int ages = 30;
+}
