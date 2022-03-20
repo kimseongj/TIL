eb24980bc085c9280b45dbd9228708e4bdedb01f
name <ksj8277@naver.com>
2022-03-20 15:40:41 +0900

    day8 TIL

---
+++ cpp/day8/class.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class book // 클래스 만드는 예시
+{
+    private:
+    int current_page_; //멤버 변수
+    void set_percent();//멤버 함수
+    public:
+    int total_page_;
+
+};
+main() {
+    
+}
