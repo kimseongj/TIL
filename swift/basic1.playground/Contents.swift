2478b2ecb0cc06fe7c4edd217c62b4ab40d81356
kimseongj <88870642+kimseongj@users.noreply.github.com>
2022-03-29 23:09:21 +0900

    day1
    swift study day1

---
+++ swift/basic1.playground/Contents.swift
@@ -0,0 +1,32 @@
+import Swift
+
+let age : Int = 10
+
+"안녕하세요! 저는 \(age)살입니다"
+//=="안녕하세요! 저는 10살입니다"
+
+"안녕하세요! 저는 \(age + 5)살입니다"
+//=="안녕하세요! 저는 15살입니다"
+
+print("안녕하세요! 저는 \(age+5)살입니다")
+
+print("\n####################\n")
+
+class Person {
+    var name : String = "Kim"
+    var age : Int = 26
+}
+
+let kim : Person = Person()
+
+print(kim)
+print("\n##############\n")
+
+dump(kim)
+
+
+//생각해보기
+var name : string = "yagom"
+var numverToAdd = int 5
+let pi = 3.14159
+var maxItemCount = 1000
