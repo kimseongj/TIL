0e6358f189b4bedcc1c23bd7ed1e19178d8ac5ab
kimseongj <88870642+kimseongj@users.noreply.github.com>
2022-03-30 21:21:46 +0900

    day2
    데이터 타입 공부 !

---
+++ swift/basic3:datatype.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//Bool
+var someBool : Bool = true
+someBool = false
+
+var someInt : Int = -100
+print(someInt)
+
+var someUInt : uint = 100
+print(someUInt)
+
+var someFloat : Float = 3.14
+print(someFloat)
+
+var someDouble : Double = 3
+print(someDouble)
+
+var someCharacter : Character = "1"
+print(someCharacter)
+//유니코드를 사용하기 때문에 유니코드로 만들어진 모든것 표현 가능(이모티콘 또한 가능) 대박
+
+
+var someString : String = "123asd"
+print(someString)
+
