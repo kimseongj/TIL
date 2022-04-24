042c6318fbe607fdb9c664205e1b5dc7ec0f10cb
kimseongj <88870642+kimseongj@users.noreply.github.com>
2022-04-24 17:02:38 +0900

    review arr/dic/set

---
+++ swift/review/review::arr:dic:set.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Swift
+
+var integers: Array<Int> = Array<Int>()
+
+var integers1: [Int] = [Int]()
+
+var integers2: [Int] = [Int]
+
+var integers3 = [Int]()
+
+integers.append(1)
+integers.append(20)
+
+print(integers)
+
+print(integers.contains(1))
+print(integers.contains(20))
+
+integers.remove(at: 0)
+
+
+var Dic : Dictionary<String, Any> = [String: Any]()
+var Dic1 : [String: Any] = Dictionary<String: Any>()
+var Dic2 : [Stirng: Any] = [String: Any]()
+var Dic3 = [String: Any]()
+
+
+var se: Set<Int> = Set<Int>()
+
+let se1: Set<Int> = [1,2,3,4,5]
+
+
+
